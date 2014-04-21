371a2c1421fceb15e529d8d91c343796003343d3
timonium694 <ttoth@student.neumont.edu>
2014-04-21 09:37:54 -0600

    Added CRUD operations

---
+++ hellorest/src/main/java/edu/neumont/csc380/hello/service/User.java
@@ -0,0 +1,35 @@
+package edu.neumont.csc380.hello.service;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private int id;
+	private String username;
+	private String password;
+	private String authLevel;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAuthLevel() {
+		return authLevel;
+	}
+	public void setAuthLevel(String authLevel) {
+		this.authLevel = authLevel;
+	}
+
+}
