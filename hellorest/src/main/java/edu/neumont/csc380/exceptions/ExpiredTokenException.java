232b10106f978f79b3d7a9eea97faa70354574e8
unknown <rmiller@student.neumont.edu>
2014-04-28 19:22:05 -0600

    for da pull

--- hellorest/src/main/java/edu/neumont/csc380/exceptions/ExpiredTokenException.java
+++
@@ -1,9 +0,0 @@
-package edu.neumont.csc380.exceptions;
-
-public class ExpiredTokenException extends Exception {
-	private static String message = "The token has expired";
-	
-	public ExpiredTokenException() {
-		super(message);
-	}
-}
